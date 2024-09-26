fdb50e270f6b74b29ad448b05fc1d0dc7767f62b
mohammadbarakat950 <40238983@hruc.ac.uk>
2024-09-26 10:11:29 +0100

    Update PythonApplication2.py

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,11 +1,10 @@
-def bubbl_sort(arr):
+def bubble_sort(arr):
    n = len(arr)
-   for i in rang(n):
-      for j in rang(0, n-i-1):
-         if arr [j] > arr [j+1]:
-            arr[j], arr[j+1] = arr[j+1], arr[j]
+   for i in range(n):
+       for j in range(0, n-i-1):
+           if arr[j] > arr[j+1]:
+               arr[j], arr[j+1] = arr[j+1], arr[j]
    return arr
-
 arr = [64,34,25,12,22,11,90]
 sorted_arr = bubble_sort(arr)
 print("sorted array is:", sorted_arr)
